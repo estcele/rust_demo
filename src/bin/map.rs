8460eedde3b25e81108444d6013b6447884c598e
张家峰 <estcele@zhangjiafengdeMacBook-Air.local>
2025-02-28 23:11:53 +0800

    Copilot自动将英文注释全部改为中文

--- src/bin/map.rs
+++ src/bin/map.rs
@@ -1,36 +1,36 @@
 use std::collections::HashMap;
 
 fn main() {
-    // Creating a new HashMap
+    // 创建一个新的 HashMap
     let mut scores = HashMap::new();
 
-    // Inserting values into the HashMap
+    // 向 HashMap 中插入值
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
 
-    // Accessing values in the HashMap
+    // 访问 HashMap 中的值
     let team_name = String::from("Blue");
     let score = scores.get(&team_name);
     match score {
-        Some(&score) => println!("Score for {}: {}", team_name, score),
-        None => println!("No score found for {}", team_name),
+        Some(&score) => println!("{} 的分数: {}", team_name, score),
+        None => println!("未找到 {} 的分数", team_name),
     }
 
-    // Iterating over key-value pairs in the HashMap
+    // 遍历 HashMap 中的键值对
     for (key, value) in &scores {
         println!("{}: {}", key, value);
     }
 
-    // Updating a value in the HashMap
+    // 更新 HashMap 中的值
     scores.insert(String::from("Blue"), 25);
-    println!("Updated score for Blue: {}", scores.get("Blue").unwrap());
+    println!("更新后 Blue 的分数: {}", scores.get("Blue").unwrap());
 
-    // Only inserting a value if the key has no value
+    // 只有在键没有值的情况下插入值
     scores.entry(String::from("Green")).or_insert(30);
-    println!("Score for Green: {}", scores.get("Green").unwrap());
+    println!("Green 的分数: {}", scores.get("Green").unwrap());
 
-    // Updating a value based on the old value
+    // 根据旧值更新值
     let yellow_score = scores.entry(String::from("Yellow")).or_insert(50);
     *yellow_score += 10;
-    println!("Updated score for Yellow: {}", scores.get("Yellow").unwrap());
+    println!("更新后 Yellow 的分数: {}", scores.get("Yellow").unwrap());
 }
